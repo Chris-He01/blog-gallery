3ff3991f6d6c664e64c53337eddca9efb2f5e1be
hehongge <hehongge@kuaishou.com>
2026-03-08 01:50:16 +0800

    feat: tibet

--- exampleSite/content/story/index.md
+++ exampleSite/content/story/index.md
@@ -10,16 +10,10 @@ resources:
     params:
       weight: 1
   
-  - src: marshal-art.mp4
-    title: "武术练习"
-    params:
-      weight: 2
-      caption: "日常的武术训练片段"
-  
   - src: martin-martz-wRuhOOaG-Z4-unsplash copy.jpg
     title: "另一个瞬间"
     params:
-      weight: 3
+      weight: 2
 ---
 
 ## 2024年3月6日
@@ -29,13 +23,6 @@ resources:
 今天天气很好，走在街上看到一只猫。它坐在那里晒太阳，很安静。让我想起小时候家里养的那只橘猫。生活就是这样，一些小小的瞬间，能让人停下来思考。阳光透过树叶，斑驳地洒在地面上。时间仿佛在这一刻静止了，只有微风轻轻吹过。我站在那里，看着它，想着很多事情。关于过去，关于未来，关于那些已经失去和还未到来的。
 
 城市的喧嚣在这一刻都远去了。只剩下这只猫，这片阳光，还有我。或许这就是生活最美好的时刻——当你停下脚步，用心去感受周围的一切。不需要太多言语，不需要刻意追寻，一切都那么自然而然。
-
-<video controls style="max-width: 100%; border-radius: 8px; margin: 2rem 0;">
-  <source src="marshal-art.mp4" type="video/mp4">
-  您的浏览器不支持视频播放。
-</video>
-
-下午去公园练了一会儿武术，录了一小段视频。虽然动作还不够标准，但每天坚持练习，总会有进步的。
 
 ![另一个瞬间](martin-martz-wRuhOOaG-Z4-unsplash.jpg)
 
--- exampleSite/pull-images.sh
+++ exampleSite/pull-images.sh
@@ -3,55 +3,74 @@
 # Home
 wget --no-clobber --content-disposition --directory-prefix=content "https://unsplash.com/photos/wRuhOOaG-Z4/download?&force=true&w=1920"
 
-# Animals
-wget --no-clobber --content-disposition --directory-prefix=content/animals "https://unsplash.com/photos/UC1pzyJFyvs/download?&force=true&w=1920"
-
-# Animals/Cats
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/gKXKBY-C-Dk/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/75715CVEJhI/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/mJaD10XeD7w/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/CEx86maLUSc/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/yMSecCHsIBc/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/rW-I87aPY5Y/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/p6yH8VmGqxo/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/LEpfefQf4rU/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/cats "https://unsplash.com/photos/nKC772R_qog/download?&force=true&w=1920"
-
-exiftool -ImageDescription="Brown tabby cat on white stairs by Alexander London" content/animals/cats/alexander-london-mJaD10XeD7w-unsplash.jpg
-exiftool -ImageDescription="selective focus photography of orange and white cat on brown table by Amber Kipp" content/animals/cats/amber-kipp-75715CVEJhI-unsplash.jpg
-
-# Animals/Dogs
-wget --no-clobber --content-disposition --directory-prefix=content/animals/dogs "https://unsplash.com/photos/Sg3XwuEpybU/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/dogs "https://unsplash.com/photos/Mv9hjnEUHR4/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/dogs "https://unsplash.com/photos/2l0CWTpcChI/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/animals/dogs "https://unsplash.com/photos/WX4i1Jq_o0Y/download?&force=true&w=1920"
 
 # Fashion & Beauty
 wget --no-clobber --content-disposition --directory-prefix=content/fashion-beauty "https://unsplash.com/photos/FkxXePJJH5g/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/fashion-beauty "https://unsplash.com/photos/63obHScZWZw/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/fashion-beauty "https://unsplash.com/photos/7gqnlnCTvlg/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/fashion-beauty "https://unsplash.com/photos/V94CguEmeos/download?&force=true&w=1920"
 
 # Nature
 wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/ZS_XuDZmxpM/download?&force=true&w=1920"
 wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/U7BG3FOT5r8/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/TUzsO59UFpo/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/P45gR9kH0SM/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/k_PbdrEs79g/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/4f8u5mFGKjw/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/f_C_lFxqThI/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/x7CyIC2jUaE/download?&force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/nature "https://unsplash.com/photos/RuaRTaKi-D4/download?force=true&w=1920"
+exiftool -ImageDescription="nature that makes you feel like you are in a dream by Daria Shevtsova" content/nature/daria-shevtsova-f_C_lFxqThI-unsplash.jpg
+exiftool -ImageDescription="aerial photography of body of water surrounded by trees during daytime by Daria Shevtsova" content/nature/daria-shevtsova-k_PbdrEs79g-unsplash.jpg
+
 
 # Private
 wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/gRknIewfaBs/download?force=true&w=1920"
 wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/QQ0naV2n-mg/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/_lQgFjsTgEs/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/7pYqHNp37Pg/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/6dH1__uRYtg/download?force=true&w=1920"
-wget --no-clobber --content-disposition --directory-prefix=content/private "https://unsplash.com/photos/t2WImwH1Fa0/download?force=true&w=1920"
 
 # Featured
 wget --no-clobber --content-disposition --directory-prefix=content/featured-album "https://unsplash.com/photos/fcwZsjyqp0s/download?force=true&w=1920"
 wget --no-clobber --content-disposition --directory-prefix=content/featured-album "https://unsplash.com/photos/IFlBNNOLHUo/download?force=true&w=1920"
 wget --no-clobber --content-disposition --directory-prefix=content/featured-album "https://unsplash.com/photos/pjszS6Q2g_Y/download?force=true&w=1920"
+
+# Tibet
+while IFS= read -r url; do
+	wget --no-clobber --content-disposition --directory-prefix=content/tibet "$url"
+done <<'EOF'
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0232.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0235.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0268.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0269.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0289.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0302.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0315.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0316.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0318.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0320.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0376.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0418.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0427.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0434.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0435.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0439.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0454.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0455.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0461.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0505.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0508.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0533.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0542.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0544.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0556.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0565.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0569.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0575.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0607.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0628.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0644.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0647.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0652.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0653.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0656.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0660.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0665.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0668.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0670.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0677.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0679.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0683.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0701.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0705.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0708.JPG
+http://tbiyvdh13.hd-bkt.clouddn.com/tibet/IMG_0716.JPG
+EOF
